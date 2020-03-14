900b7848ca302169e55729a236fb4f098ed918e4
shibudaniel <47894492+shibuadaniel@users.noreply.github.com>
2020-03-14 15:32:05 -0400

    login first commit

--- XcodeTest/AppDelegate.swift
+++ XcodeTest/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
