d73f537c78801387d403ff3c563f97add1e098d0
shibudaniel <47894492+shibuadaniel@users.noreply.github.com>
2020-03-14 15:09:50 -0400

    Initial Commit

---
+++ XcodeTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeTest
+//
+//  Created by SHIBU DANIEL on 3/14/20.
+//  Copyright © 2020 Skie Design Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
